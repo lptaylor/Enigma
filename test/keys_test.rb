bf8bd8e792e89115d0346027d8b14cd956a39d2c
Lance Taylor <lance.taylor98@hotmail.com>
2018-11-03 14:33:32 -0600

    adds class method generate test

--- test/keys_test.rb
+++ test/keys_test.rb
@@ -2,14 +2,21 @@ require 'simplecov'
 SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/keys'
+require './lib/key'
 
-class KeysTest < Minitest::Test
+class KeyTest < Minitest::Test
 
   def test_it_exists
-    keys = Keys.new
+    key = Key.new
 
-    assert_instance_of Keys, keys
+    assert_instance_of Key, key
+  end
+
+  def test_it_has_a_class_method_that_will_generate_a_number
+    key = Key.new
+    key.generate
+
+    assert_instance_of Fixnum, key_array[0]
   end
 
 end
