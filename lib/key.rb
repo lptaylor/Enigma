45707c17488e8967e909a07a98fb7c60212da595
Lance Taylor <lance.taylor98@hotmail.com>
2018-11-03 23:06:27 -0600

    adds correctly working key generator

--- lib/key.rb
+++ lib/key.rb
@@ -6,8 +6,14 @@ class Key
   end
 
   def generate
-    until @key_array.length == 5
-      @key_array << rand(10)
+    base_array = Array.new
+    until base_array.length == 5
+      base_array << rand(10)
+    end
+    combined_array = base_array.zip(base_array[1..4])
+    interm_key_array = combined_array[0..3]
+    @key_array = interm_key_array.map do |num|
+      num.join.to_i
     end
   end
 
