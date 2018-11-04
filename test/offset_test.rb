331a62c1fed59d5000eccd4ddb5bb13d8613d297
Lance Taylor <lance.taylor98@hotmail.com>
2018-11-03 21:16:39 -0600

    adds all testing assumed needed for offset class

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -9,4 +9,22 @@ class OffsetTest < Minitest::Test
 
     assert_instance_of Offset, offset
   end
+
+  def test_it_can_take_in_a_date
+    offset = Offset.new("041118")
+
+    assert_equal "041118", offset.date
+  end
+
+  def test_it_defalts_to_todays_date
+    offset = Offset.new
+
+    assert_equal "031118", offset.date
+  end
+
+  def test_it_can_generate_the_offset
+    offset = Offset.new
+
+    assert_equal 0, offset.offset_array
+  end
 end
