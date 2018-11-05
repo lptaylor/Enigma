50e2f85f96023d3f01479ab9eefec651eee58e08
Lance Taylor <lance.taylor98@hotmail.com>
2018-11-05 15:31:36 -0700

    adds working encrypt runner

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -4,20 +4,12 @@ message, encrypted = ARGV
 message = File.open(message, 'r')
 encrypted = File.new(encrypted, 'w')
 enigma = Enigma.new
-key = Key.new.generate
+key = Key.new.make_base_array.join
 date = Date.today.strftime('%d%m%y')
 File.foreach(message) do |line|
   encrypt = enigma.encrypt(line, key, date)
   encrypted.puts(encrypt[:encryption])
 end
-
-
-
-
-
-enigma.encrypt(message)
-
-
 
 
 p "Created 'encrypted.txt' with the key #{key} and #{date}"
