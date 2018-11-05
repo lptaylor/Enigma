8dbf422fe75a7961e29b97b07ffab86f49d26bd4
Lance Taylor <lance.taylor98@hotmail.com>
2018-11-04 20:06:07 -0700

    adds test to shift alphabet in enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,9 +5,17 @@ class Enigma
 
   def initialize
     @shift_array = Array.new
+    @alphabet = ("a".."z").to_a << " "
   end
 
   def encrypt(message, key, date = Date.today.strftime('%d%m%y'))
     shifter = Shifter.new(key, date)
+    @shift_array = shifter.get_shift_array
+    message_indexed = message.chars.map do |letter|
+                        @alphabet.find_index(letter.downcase)
+                      end
+    message_indexed
   end
+
+
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,6 +14,15 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_can_encyrpt_a_message_with_no_supplied_key_or_offset
+    enigma = Enigma.new
 
+    assert_equal "", enigma.encrypted
+  end
+
+  def test_it_can_shift_the_alphabet
+    enigma = Enigma.new
+    enigma.shift_alphabet
+
+    assert_equal "bdf", enigma.shifted_alphabet[0..2].join
   end
 end
